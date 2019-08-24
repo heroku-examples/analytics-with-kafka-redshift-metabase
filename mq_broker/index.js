305d974f925d933f8811733fe5dbbefdeceb9039
Dylan Staley <staley.dylan@gmail.com>
2019-08-23 20:43:11 -0700

    write queue length to kafka

--- mq_broker/index.js
+++ mq_broker/index.js
@@ -5,7 +5,7 @@ const constants = require("./constants");
 const mqUrl = process.env.CLOUDAMQP_URL || "amqp://localhost";
 const queue = "tasks";
 
-const consumer = new Kafka.SimpleConsumer({
+const kafkaConfig = {
   idleTimeout: 1000,
   connectionTimeout: 10 * 1000,
   clientId: "mq_broker",
@@ -16,12 +16,17 @@ const consumer = new Kafka.SimpleConsumer({
       key: process.env.KAFKA_CLIENT_CERT_KEY
     }
   }
-});
+};
+
+const consumer = new Kafka.SimpleConsumer(kafkaConfig);
+const producer = new Kafka.Producer(kafkaConfig);
 
 (async () => {
   const mqConn = await mqClient.connect(mqUrl);
   const chan = await mqConn.createChannel();
   await chan.assertQueue(queue);
+
+  await producer.init();
 
   await consumer.init();
   await consumer.subscribe(constants.KAFKA_TOPIC, messageSet => {
@@ -30,5 +35,15 @@ const consumer = new Kafka.SimpleConsumer({
       chan.sendToQueue(queue, new Buffer.from(value));
       console.log(`Sent message to mq: ${value}`);
     }
+
+    chan.assertQueue(queue).then(info => {
+      const value = JSON.stringify({ length: info.messageCount });
+      producer.send({
+        topic: constants.KAFKA_QUEUE_TOPIC,
+        message: { value },
+        partition: 0
+      });
+      console.log(`Message queue info: ${value}`);
+    });
   });
 })();
