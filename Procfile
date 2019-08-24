5c321575048ea1a8c74cd6ad30be7c739ffe7c0e
Dylan Staley <staley.dylan@gmail.com>
2019-08-23 19:59:58 -0700

    create mq broker

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 redshift_batch: cd redshift_batch && node index.js
 web: cd viz && npm run start
 dataworker: cd generate_data && node index.js -c kafka.js
+mq_broker: cd mq_broker && node index.js
---
+++ mq_broker/index.js
@@ -0,0 +1,31 @@
+const Kafka = require("no-kafka");
+
+const mqUrl = process.env.CLOUDAMQP_URL || "amqp://localhost";
+const queue = "tasks";
+
+const consumer = new Kafka.SimpleConsumer({
+  idleTimeout: 1000,
+  connectionTimeout: 10 * 1000,
+  clientId: "mq_broker",
+  consumer: {
+    connectionString: process.env.KAFKA_URL.replace(/\+ssl/g, ""),
+    ssl: {
+      cert: process.env.KAFKA_CLIENT_CERT,
+      key: process.env.KAFKA_CLIENT_CERT_KEY
+    }
+  }
+});
+
+(async () => {
+  const mqConn = await mqClient.connect(mqUrl);
+  const chan = mqConn.createChannel();
+  await chan.assertQueue(queue);
+
+  await consumer.init();
+  await consumer.subscribe(constants.KAFKA_WEIGHT_TOPIC, messageSet => {
+    messageSet.map(({ message }) => {
+      chan.sendToQueue(queue, message);
+    });
+    console.log(`Sent ${messageSet.length} messages to mq.`);
+  });
+})();
