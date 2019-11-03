74e41434c25d8838e067005a3b2afcd9dc3ebe5a
Naomi Suzuki <naomi.suzuki@meltmedia.com>
2019-11-03 13:35:04 +0100

    moved files

--- viz/src/demand/DemandControls.js
+++ viz/src/demand/DemandControls.js
@@ -6,7 +6,6 @@ import demandConstants from './demandConstants'
 
 export default class DemandControls {
   constructor(options) {
-
     if (document.querySelectorAll('.order-control-buttons').length > 0) {
       require('./order-control')()
     }
---
+++ viz/src/demand/order-control.js
@@ -0,0 +1,37 @@
+module.exports = () => {
+  const $ = require('jQuery')
+
+  let ajaxing = false
+
+  const generateAjaxHandler = (command) => {
+    return (e) => {
+      e.preventDefault()
+      if (ajaxing) {
+        return
+      }
+      ajaxing = true
+
+      $.ajax({
+        type: 'POST',
+        url: '/demand/command',
+        data: JSON.stringify({ command }),
+        dataType: 'json',
+        contentType: 'application/json'
+      })
+        .then((data) => {
+          console.log(data)
+          ajaxing = false
+          alert(`${command} order has been sent. Please check the log.`)
+        })
+        .catch((e) => {
+          console.log(e)
+          ajaxing = false
+          alert('Error: ' + e)
+        })
+    }
+  }
+
+  ;['start', 'stop', 'reset'].forEach((key) => {
+    $(`[${key}-button]`).click(generateAjaxHandler(key))
+  })
+}
